e339d21e6a8c04a702f401c92a3e30beebbc3148
MateoB00 <mateobianco91100@gmail.com>
2024-10-14 15:35:06 +0200

    add urls for each resource

--- Exercice_1/config/urls.py
+++ Exercice_1/config/urls.py
@@ -15,8 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    AuteurViewSet,
+    LivreViewSet,
+    CategorieViewSet,
+    ExemplaireViewSet,
+    EmpruntViewSet,
+    CommentaireViewSet,
+    EditeurViewSet,
+    EvaluationViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'auteurs', AuteurViewSet)
+router.register(r'livres', LivreViewSet)
+router.register(r'categories', CategorieViewSet)
+router.register(r'exemplaires', ExemplaireViewSet)
+router.register(r'emprunts', EmpruntViewSet)
+router.register(r'commentaires', CommentaireViewSet)
+router.register(r'editeurs', EditeurViewSet)
+router.register(r'evaluations', EvaluationViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
