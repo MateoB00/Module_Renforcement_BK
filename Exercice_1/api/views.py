43bebdfbc261bd51edb1ba2d50ff97450c24bd10
MateoB00 <mateobianco91100@gmail.com>
2024-10-14 15:31:59 +0200

    add views

--- Exercice_1/api/views.py
+++ Exercice_1/api/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Auteur, Categorie, Exemplaire, Emprunt, Commentaire, Editeur, Evaluation
+from .serializers import (
+    AuteurSerializer,
+    CategorieSerializer,
+    ExemplaireSerializer,
+    EmpruntSerializer,
+    CommentaireSerializer,
+    EditeurSerializer,
+    EvaluationSerializer
+)
 
-# Create your views here.
+class AuteurViewSet(viewsets.ModelViewSet):
+    queryset = Auteur.objects.all()
+    serializer_class = AuteurSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CategorieViewSet(viewsets.ModelViewSet):
+    queryset = Categorie.objects.all()
+    serializer_class = CategorieSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ExemplaireViewSet(viewsets.ModelViewSet):
+    queryset = Exemplaire.objects.all()
+    serializer_class = ExemplaireSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class EmpruntViewSet(viewsets.ModelViewSet):
+    queryset = Emprunt.objects.all()
+    serializer_class = EmpruntSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CommentaireViewSet(viewsets.ModelViewSet):
+    queryset = Commentaire.objects.all()
+    serializer_class = CommentaireSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class EditeurViewSet(viewsets.ModelViewSet):
+    queryset = Editeur.objects.all()
+    serializer_class = EditeurSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class EvaluationViewSet(viewsets.ModelViewSet):
+    queryset = Evaluation.objects.all()
+    serializer_class = EvaluationSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
