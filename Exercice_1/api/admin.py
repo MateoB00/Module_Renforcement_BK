cf0e547fb6e894b5cdeeb021c97ca8e13b62aff0
MateoB00 <mateobianco91100@gmail.com>
2024-10-14 14:59:33 +0200

    add gitignore, create super user

--- Exercice_1/api/admin.py
+++ Exercice_1/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from api import models
 
-# Register your models here.
+admin.site.register(models.Auteur)
+admin.site.register(models.Livre)
+admin.site.register(models.Categorie)
+admin.site.register(models.Exemplaire)
+admin.site.register(models.Emprunt)
+admin.site.register(models.Commentaire)
+admin.site.register(models.Editeur)
+admin.site.register(models.Evaluation)
+
